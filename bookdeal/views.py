7973e0e47d48501c69f7a07ef7234b745d4f6612
BXYMartin <BXYMartin@hotmail.com>
2018-11-22 10:25:33 +0800

    Add PriceTag

--- bookdeal/views.py
+++ bookdeal/views.py
@@ -75,7 +75,7 @@ def addbook(request):
         price = float(request.POST.get('price'))
         cover = request.FILES.get('cover')
         if book_name == "" or len(info) < 10 or price > 10000 or price < 0:
-            return render(request, 'panel/index.html', {'TYPE':"Failure", 'msg':"addbook", "username": request.user.username})
+            return render(request, 'panel/index.html', {'TYPE':"Failure", 'msg':"Way too expensive and too little info given!", "username": request.user.username})
         if cover is None or cover.name.split('.')[1].lower() not in ['jpeg', 'jpg', 'png'] or cover.size > 10000000:
             return render(request, 'panel/index.html', {'TYPE': "Warning", 'msg': "illegal cover", "username":request.user.username})
         Book.objects.create(name=book_name, info=info, price=price, cover=cover, owner=request.user)
