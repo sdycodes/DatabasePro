0bfc671b08be4b1d86af715870d654cb2e2eca4b
sdyyyy <s.dy@foxmail.com>
2018-12-02 22:29:43 +0800

    fix login

--- DatabasePro/urls.py
+++ DatabasePro/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.panel),
+    url(r'^$', views.login),
     url('front/', views.front),
     url('panel/', views.panel),
     url('detail/', views.detail_report),
